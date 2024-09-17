2c1d0ccccb99c3b8a946d52df4423c64199fd90b
sengutengu <seungkim.h@gmail.com>
2024-09-17 11:15:32 -0400

    Change routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Navbar } from "./Components/Navbar";
-import { Home, Interests, Projects, Resume} from "./Components/Pages/";
+import { Home, Interests, Projects, Resume } from "./Components/Pages/";
 
 function App() {
-  return (<div className="App">
-    <Navbar />
-    <Routes>
-      <Route path="/home" element={<Home />}></Route>
-      <Route path="/interests" element={<Interests />}></Route>
-      <Route path="/projects" element={<Projects />}></Route>
-      <Route path="/resume" element={<Resume />}></Route>
-    </Routes>
-  </div>
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/navbar/home" element={<Home />}></Route>
+        <Route path="/navbar/interests" element={<Interests />}></Route>
+        <Route path="/navbar/projects" element={<Projects />}></Route>
+        <Route path="/navbar/resume" element={<Resume />}></Route>
+      </Routes>
+    </div>
   );
-};
+}
 
 export default App;
